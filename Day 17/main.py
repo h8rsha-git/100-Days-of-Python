568625f125b7f11231a2b2c48358902cd7fc0479
h8rsha-git <h8rsha@gmail.com>
2022-07-26 00:40:50 +0530

    commit

--- Day 17/main.py
+++ Day 17/main.py
@@ -1,10 +1,22 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
+# from dict to object
 q_data = question_data
-list_of_questions = []
+question_bank = []
 
-for item in range(0, len(question_data)):
-    list_of_questions.append(Question(item["text"], item["answer"]))
+# for item in range(0, len(question_data)):
+#     question_bank.append(Question(q_data[item]["text"], q_data[item]["answer"]))
 
-print(list_of_questions)
+for question in question_data[0]["results"]:
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
+
+quiz = QuizBrain(question_bank)
+while quiz.still_has_questions():
+    quiz.next_question()
+print("You have completed the quiz.")
+print(f"Your final score: {quiz.score}/{quiz.question_number}")
